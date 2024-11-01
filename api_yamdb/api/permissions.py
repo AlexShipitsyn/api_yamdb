3eb7031a9f50cd4937d09a75b36f9cc4487c23df
Глеб Лукьянов <lukyanovgleb2703@yandex.ru>
2024-11-01 11:08:58 +0700

    permission

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -2,4 +2,10 @@ from rest_framework import permissions
 
 
 class IsAuthorOrModeratorOrAdmin(permissions.BasePermission):
-    pass
+    def has_object_permission(self, request, view, obj):
+        return bool(
+            request.method in permissions.SAFE_METHODS
+            or obj.author == request.user
+            or request.user.role == 'moderator'
+            or request.user.is_staff
+        )

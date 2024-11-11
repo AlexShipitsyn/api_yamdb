067233ca4a752d456eba495014e6481e4a3e7fd8
Глеб Лукьянов <lukyanovgleb2703@yandex.ru>
2024-11-11 13:15:52 +0700

    upgrate send_mail

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -1,9 +1,11 @@
 from django.core import mail
+from django.conf import settings
 from django.contrib.auth.tokens import default_token_generator as dtg
 
 
 def send_mail(user):
     subject = 'Confirmation code'
     to = user.email
+    from_email = settings.DEFAULT_FROM_EMAL
     text_content = f'Confirmation code: {dtg.make_token(user)}'
-    mail.send_mail(subject, text_content, None, [to])
+    mail.send_mail(subject, text_content, from_email, [to])
